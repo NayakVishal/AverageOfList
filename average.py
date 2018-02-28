94df2978709dba2bc03f2e4ca146e68cceb79956
NayakVishal <36918693+NayakVishal@users.noreply.github.com>
2018-02-28 16:01:02 +0530

    Added average script and a sample input file

---
+++ average.py
@@ -0,0 +1,8 @@
+data = []
+with open(r'test.txt') as f:
+    for line in f:
+        fields = line.split()
+        rowdata = map(float, fields)
+        data.extend(rowdata)
+
+print(sum(data)/len(data))
